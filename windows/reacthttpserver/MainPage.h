e600c0508c4bdcbcb113d08c50a4c7cde979b976
cocoalix <cocoalix@gmail.com>
2021-12-30 19:02:56 +0900

    first commit

---
+++ windows/reacthttpserver/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reacthttpserver::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reacthttpserver::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
